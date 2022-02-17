1028326fa7b39c0c0029c72410165e325794371e
Jhector10 <jashsmash10@gmail.com>
2022-02-17 02:26:32 -0500

    work on stair climber

--- PAs/PA3-Stair/stairclimber.cpp
+++ PAs/PA3-Stair/stairclimber.cpp
@@ -17,6 +17,28 @@ vector<vector<int> > get_ways(int num_stairs) {
     // TODO: Return a vector of vectors of ints representing
     // the different combinations of ways to climb num_stairs
     // stairs, moving up either 1, 2, or 3 stairs at a time.
+
+    vector<vector<int> > ways;
+    vector<vector<int> > path;
+
+    if (num_stairs <= 0) {
+        ways.push_back(vector<int>());
+    }
+    
+    for (int i = 1; i < 4; i++) {
+        if (num_stairs >= i) {
+            path = get_ways(num_stairs - 1);
+            for (int j = 0; j < int(ways.size()); j++) {
+                path[j].insert(path[j].begin(), i);
+            }
+            for (int n = 0; n < int(ways.size()); n++) {
+                ways.push_back(path[n]);
+            }
+        }
+    }
+
+    return ways;
+
 }
 
 void display_ways(const vector< vector<int> > &ways) {

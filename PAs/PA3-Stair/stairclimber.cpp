e898ec7cb1a3dbfd74f456e6f18f91465bc8116a
Jhector10 <jashsmash10@gmail.com>
2022-02-20 00:23:00 -0500

    work on stair climber

--- PAs/PA3-Stair/stairclimber.cpp
+++ PAs/PA3-Stair/stairclimber.cpp
@@ -23,16 +23,14 @@ vector<vector<int> > get_ways(int num_stairs) {
 
     if (num_stairs <= 0) {
         ways.push_back(vector<int>());
-    }
-    
-    for (int i = 1; i < 4; i++) {
-        if (num_stairs >= i) {
-            path = get_ways(num_stairs - 1);
-            for (int j = 0; j < int(ways.size()); j++) {
-                path[j].insert(path[j].begin(), i);
-            }
-            for (int n = 0; n < int(ways.size()); n++) {
-                ways.push_back(path[n]);
+    } else {
+        for (int i = 1; i < 4; i++) {
+            if (num_stairs >= i) {
+                path = get_ways(num_stairs - 1);
+                for (size_t j = 0; j < path.size(); j++) {
+                    path[j].insert(path[j].begin(), i);
+                }
+                ways.insert(ways.end(), path.begin(), path.end());
             }
         }
     }
@@ -41,11 +39,67 @@ vector<vector<int> > get_ways(int num_stairs) {
 
 }
 
+int num_digits(size_t num) {
+    // Helper from Sieve hw: write code to determine how many digits are in an integer
+    int digits = 0;
+
+    while(num != 0) {
+        num /= 10;
+        digits += 1;
+    }
+
+    return digits;
+}
+
 void display_ways(const vector< vector<int> > &ways) {
     // TODO: Display the ways to climb stairs by iterating over
     // the vector of vectors and printing each combination.
+
+    size_t width = ways.size();
+    int digits = num_digits(width);
+
+    for(size_t i = 0; i < ways.size(); i++) {
+        cout << setw(digits) << (i + 1) << ". [";
+        for (size_t j = 0; j < ways[i].size(); j++) {
+            if (j == ways[i].size() - 1) {
+                cout << ways[i][j] << "]" << endl;;
+            } else {
+                cout << ways[i][j] << ", ";
+            }
+        }
+    }
+    
 }
 
 int main(int argc, char * const argv[]) {
+
+    int numberOfStairs;
+    vector< vector<int> > ways;
+
+    if(argc != 2) {
+        // user gave the wrong number of command line arguments
+        cerr << "Usage: " << argv[0] << " <number of stairs>" << endl;
+        return 1;
+    } 
+
+    // Use stringstream for conversion. Don't forget to #include <sstream>
+    istringstream iss(argv[1]);
+
+    // Check for error.
+    if ( !(iss >> numberOfStairs) || numberOfStairs < 1 ) {
+        cerr << "Error: Number of stairs must be a positive integer." << endl;
+        return 1;
+    }
+
+    ways = get_ways(numberOfStairs);
+
+    if (numberOfStairs == 1) { // check the inside of the vector to see if it's only one stair
+        cout << "1 way to climb 1 stair." << endl;
+    } else { // prints out a starter sentence stating it's multiple stairs
+    	cout << ways.size() << " ways to climb " << numberOfStairs << " stairs." << endl;
+    }
+
+    display_ways(get_ways(numberOfStairs));
+    return 0;
 
 }

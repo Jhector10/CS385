b28d521ec2bf35483fdbaf486b2cb082eccdf9c0
Jhector10 <jashsmash10@gmail.com>
2022-03-23 13:02:21 -0400

    inversion work

---
+++ PAs/PA5-Inversion/inversioncounter.cpp
@@ -0,0 +1,21 @@
+/*******************************************************************************
+ * Name        : inversioncounter.cpp
+ * Author      : Joshua Hector
+ * Version     : 1.0
+ * Date        : March 27th, 2022
+ * Description : Counts the number of inversions in an array.
+ * Pledge      : I pledge my honor that I have abided by the Stevens Honor System.
+ ******************************************************************************/
+#include <iostream>
+#include <algorithm>
+#include <sstream>
+#include <vector>
+#include <cstdio>
+#include <cctype>
+#include <cstring>
+
+using namespace std;
+
+int main(int argc, char *argv[]) {
+    return 0;
+}
